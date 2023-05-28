2c3154d0f37c5098c33a44744eddc652ae1d8895
Ionut Gherman <ghermanionut96@gmail.com>
2023-05-28 14:21:33 +0300

    pantah: enable SystemUI blur

--- device-lineage.mk
+++ device-lineage.mk
@@ -151,3 +151,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.6.vendor:64 \
     com.google.hardware.pixel.display-V4-ndk.vendor:64 \
     com.google.hardware.pixel.display-V5-ndk.vendor
+
+# Enable SystemUI blur
+TARGET_ENABLE_BLUR := true
